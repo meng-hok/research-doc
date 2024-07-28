f76476fe522b8aa0098b3f201df123788c7b6f17
meng hok <menghok.sok@hanumanbeverages.com.kh>
2024-07-28 13:28:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let str = `  <ul class="nav flex-column">
+<li class="nav-item">
+    <a class="nav-link " href="index.html">
+        ទំរាំដើម <span class="sr-only">(current)</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link " href="documents.html">
+        បញ្ចូលឯកសារ
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="reports.html">
+        រាយការណ៍លទ្ធផល
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="calculations.html">
+        បញ្ជីគណនា
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link " href="calculations.html">
+        គ្រប់គ្រងគណនី
+    </a>
+</li>
+</ul>`
+// active
+$("#target_div").html(str)     
