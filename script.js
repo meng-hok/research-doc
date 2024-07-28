9cd7a59f2ec5de1789596fceefc2c98ae20c7afb
meng hok <menghok.sok@hanumanbeverages.com.kh>
2024-07-28 13:53:08 +0700

    #commitee

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-let str = `  <ul class="nav flex-column">
-<li class="nav-item">
-    <a class="nav-link " href="index.html">
-        ទំរាំដើម <span class="sr-only">(current)</span>
-    </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link " href="documents.html">
-        បញ្ចូលឯកសារ
-    </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="reports.html">
-        រាយការណ៍លទ្ធផល
-    </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="calculations.html">
-        បញ្ជីគណនា
-    </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link " href="calculations.html">
-        គ្រប់គ្រងគណនី
-    </a>
-</li>
-</ul>`
+let str = `  
+    <nav class="col-md-2 d-none d-md-block bg-default sidebar">
+        <div class="sidebar-sticky" >
+            <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link text-white " href="index.html">
+                    ទំរាំដើម <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="documents.html">
+                    បញ្ចូលឯកសារ
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="reports.html">
+                    រាយការណ៍លទ្ធផល
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="calculations.html">
+                    បញ្ជី ឯកសារ
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white " href="user.html">
+                    គ្រប់គ្រងគណនី
+                </a>
+            </li>
+            </ul>
+        </div>
+</nav>
+                    
+                    `
 // active
-$("#target_div").html(str)     
+$("#target_div").prepend(str)     
